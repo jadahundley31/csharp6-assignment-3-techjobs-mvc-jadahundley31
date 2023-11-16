d1fb9460eb17293f2cd1dd4298cd96feb781aeea
Jada <jada.hundley@health.slu.edu>
2023-11-15 20:32:13 -0600

    test task 3 passed

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -18,5 +18,20 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = new List<Job>();
+
+        if (searchType.Equals("all") || searchType.Equals(""))
+        {
+            ViewBag.jobs = JobData.FindAll();
+        }
+        else
+        {
+            ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+
+        return ViewBag.jobs;
+    }
 }
 
